856d57b99766090ab20b1c12ada1cdfe8efb8f4e
Taira18 <31222319+Taira18@users.noreply.github.com>
2017-12-18 15:08:02 -1000

    Add files via upload

--- BinaryTreeTester.java
+++ BinaryTreeTester.java
@@ -38,6 +38,48 @@ public class BinaryTreeTester {
 		System.out.println(tree.countMatches(20));
 		System.out.println(tree.maxRecord());
 		System.out.println(tree.minRecord());
+		
+		BinaryTree tree2 = new BinaryTree();
+		int[] inputData2 = {7, 12, 3, 6, 9, 15, 1, 4, 8, 10, 13, 17};
+		for (int elem : inputData2) {
+			tree2.insert(elem);
+		}
+		
+		System.out.println("In-Order Traversal");
+		tree2.inOrder();
+		System.out.println("\n\n");
+		// should be: -150, -5, 0, 12, 15, 20, 20, 25, 100, 126, 200, 1000
+		
+		System.out.println("Pre-Order Traversal");
+		tree2.preOrder();
+		System.out.println("\n\n");
+		// should be: 20, 15, -5, -150, 0, 12, 200, 25, 20, 100, 126, 1000
+		
+		System.out.println("Post-Order Traversal");
+		tree2.postOrder();
+		System.out.println("\n\n");
+		// should be: -150, 12, 0, -5, 15, 20, 126, 100, 25, 1000, 200, 20
+		
+		System.out.println("Size: " + tree2.size());
+		// should be: 12
+		
+		System.out.println("Maximum Depth: " + tree2.maxDepth());
+		//should be: 5
+		
+		System.out.println(tree2.countMatches(20));
+		System.out.println(tree2.maxRecord());
+		System.out.println(tree2.minRecord());
+		tree2.removeNode(15);
+		tree2.inOrder();
+		System.out.println("\n\n");
+
+		tree2.preOrder();		
+		System.out.println("\n\n");
+
+		tree2.postOrder();	
+		System.out.println("\n\n");
+
+		
 	}
 
 }
